66615feeda06c0e00c93ab7ba4bfd5fa4e68395f
Ryna Truong <rynatruong@outlook.com>
2020-03-18 21:50:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   attr_accessor
   attr_reader :sender, :receiver, :transfer, :pending
   
-  def initialize(sender, receiver, transfer, status)
+  def initialize(sender, receiver, transfer)
     @sender = sender
     @receiver = receiver
     @transfer 
