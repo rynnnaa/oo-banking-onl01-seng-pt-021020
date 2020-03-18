fbe6f9afbe09cd4e993a72a2e56dbe312280a2a4
Ryna Truong <rynatruong@outlook.com>
2020-03-18 21:42:35 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,9 +18,9 @@ class BankAccount
   end
   
   def valid?
-    if @status == 'open' && @balance > 0
-      true
-    else
-      false
+        if @status == 'open' && @balance > 0
+            true
+        else
+            false
+        end
     end
-  end
