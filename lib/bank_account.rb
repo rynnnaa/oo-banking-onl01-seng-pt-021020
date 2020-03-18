2ebca9a74b29dac4b81ec29402c70b5b8c7de86e
Ryna Truong <rynatruong@outlook.com>
2020-03-18 21:30:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_accessor 
   attr_reader :name, :balance, :status
   
-  def initialize(name, balance= 1000, status= "open")
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
